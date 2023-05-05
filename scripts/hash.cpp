4882368dc7e8486cf95c439ea696ca03e9ce6e62
mtss-dev <matheus.silva@icomp.ufam.edu.br>
2023-05-04 21:42:03 -0400

    Hash "incorreto" dizendo o Altigran...

--- scripts/hash.cpp
+++ scripts/hash.cpp
@@ -7,11 +7,10 @@ using namespace std;
 // Definir o tamanho do bloco e de Buckets
 const int BLOCK_SIZE = 4096;
 const int NUM_BUCKETS = 100;
-const int NUM_BLOCKS = 10;
+const int NUM_BLOCKS = 5;
 
 // Definição da estrutura do registro
 struct Registro {
-    int tamanho;
     int id;
     string title;
     int year;
@@ -19,7 +18,9 @@ struct Registro {
     int citations;
     string update;
     string snippet;
+    int tamanho;
 };
+
 
 Registro* criarRegistro(int id, string title, int year, string authors, int citations, string update, string snippet) {
     Registro* registro = new Registro();
@@ -30,11 +31,12 @@ Registro* criarRegistro(int id, string title, int year, string authors, int cita
     registro->citations = citations;
     registro->update = update;
     registro->snippet = snippet;
-    registro->tamanho = sizeof(int) + sizeof(int) + title.size() + sizeof(int) + authors.size() + sizeof(int) + update.size() + sizeof(int) + snippet.size();
+    registro->tamanho = registro->title.size() + 4 + sizeof(int) + sizeof(int) + registro->authors.size() + sizeof(int) + sizeof(int) + registro->update.size() + registro->snippet.size();
     return registro;
 }
 
 void imprimeRegistro(Registro registro) {
+    cout << "------------------------" << endl;
     cout << "ID: " << registro.id << endl;
     cout << "Titulo: " << registro.title << endl;
     cout << "Ano: " << registro.year << endl;
@@ -43,6 +45,7 @@ void imprimeRegistro(Registro registro) {
     cout << "Atualizacao: " << registro.update << endl;
     cout << "Snippet: " << registro.snippet << endl;
     cout << "Tamanho: " << registro.tamanho << endl;
+    cout << "------------------------" << endl;
 }
 
 struct BlocoCabecalho {
@@ -103,48 +106,86 @@ int hashFunction(int id){
     return index;
 }
 
+void imprimirRegistrosBloco(Bloco* bloco) {
+    int posicaoAtual = 0;
+    int blocos_lidos = 0;
+    Registro* registro = new Registro();
+
+    while (blocos_lidos < bloco->cabecalho->quantidade_registros) {
+
+        // Deserializa o próximo registro no bloco
+        memcpy(&registro->id, &bloco->dados[posicaoAtual], sizeof(int));
+        posicaoAtual += sizeof(int);
+
+        registro->title = string((char*)&bloco->dados[posicaoAtual]);
+        posicaoAtual += registro->title.size()+1;
+
+        memcpy(&registro->year, &bloco->dados[posicaoAtual], 2);
+        posicaoAtual += sizeof(int);
+
+        registro->authors = string((char*)&bloco->dados[posicaoAtual]);
+        posicaoAtual += registro->authors.size()+1;
+
+        memcpy(&registro->citations, &bloco->dados[posicaoAtual],1);
+        posicaoAtual += sizeof(int);
+
+        registro->update = string((char*)&bloco->dados[posicaoAtual]);
+        posicaoAtual += registro->update.size()+1;
+
+        registro->snippet = string((char*)&bloco->dados[posicaoAtual]);
+        posicaoAtual += registro->snippet.size()+1;
+
+        
+        registro->tamanho = registro->title.size() + sizeof(int) + registro->authors.size() + sizeof(int) + sizeof(int) + registro->update.size() + registro->snippet.size() + 4;
+
+        // Imprime o registro
+        imprimeRegistro(*registro);
+        blocos_lidos++;
+    }
+    delete registro;
+}
+
 // Função para inserir um registro no bloco
 void inserir_registro_bloco(Bloco* bloco, Registro* registro) {
-    int posicao = bloco->cabecalho->posicoes_registros.front();
-    cout << "Posicao: " << posicao << endl;
+    int posicao = bloco->cabecalho->posicoes_registros.back();
 
     // copia o valor de cada campo para a posição correta no buffer de dados
     memcpy(&bloco->dados[posicao], &registro->id, sizeof(int));
     posicao += sizeof(int);
 
-    memcpy(&bloco->dados[posicao], registro->title.c_str(), registro->title.size());
-    posicao += registro->title.size();
+    // copiar a string de titulo com o caractere nulo no final
+    memcpy(&bloco->dados[posicao], registro->title.c_str(), registro->title.size() + 1);
+    posicao += registro->title.size() + 1;
 
+    // copiar o ano
     memcpy(&bloco->dados[posicao], &registro->year, sizeof(int));
     posicao += sizeof(int);
 
-    memcpy(&bloco->dados[posicao], registro->authors.c_str(), registro->authors.size());
-    posicao += registro->authors.size();
+    // copiar a string de autores com o caractere nulo no final
+    memcpy(&bloco->dados[posicao], registro->authors.c_str(), registro->authors.size() + 1);
+    posicao += registro->authors.size() + 1;
 
+    // copiar o numero de citacoes
     memcpy(&bloco->dados[posicao], &registro->citations, sizeof(int));
     posicao += sizeof(int);
 
-    memcpy(&bloco->dados[posicao], registro->update.c_str(), registro->update.size());
-    posicao += registro->update.size();
+    // copiar a string de atualizacao com o caractere nulo no final
+    memcpy(&bloco->dados[posicao], registro->update.c_str(), registro->update.size() + 1);
+    posicao += registro->update.size() + 1;
 
-    memcpy(&bloco->dados[posicao], registro->snippet.c_str(), registro->snippet.size());
-    posicao += registro->snippet.size();
+    // copiar a string de snippet com o caractere nulo no final
+    memcpy(&bloco->dados[posicao], registro->snippet.c_str(), registro->snippet.size() + 1);
+    posicao += registro->snippet.size() + 1;
     
-
     // atualiza o cabeçalho do bloco com as informações do novo registro
     bloco->cabecalho->quantidade_registros++;
     bloco->cabecalho->tamanho_disponivel -= registro->tamanho;
     
     // adiciona a posição do novo registro à lista de posições no cabeçalho
     bloco->cabecalho->posicoes_registros.push_back(posicao);
-    cout << "Bloco pertence ao Bucket: " << hashFunction(registro->id) << endl;
-    cout << "Tamanho disponivel: " << bloco->cabecalho->tamanho_disponivel << endl;
-    cout << "Quantidade de registros: " << bloco->cabecalho->quantidade_registros << endl;
-    cout << "Posicoes dos registros: ";
-    for (int posicao : bloco->cabecalho->posicoes_registros) {
-        cout << posicao << " ";
+    if(bloco->cabecalho->quantidade_registros > 1){
+        imprimirRegistrosBloco(bloco);
     }
-    cout << endl;
 }
 
 // Função para inserir um registro na tabela hash
@@ -164,7 +205,7 @@ void inserir_registro_bucket(HashTable* hashtable, Registro* registro) {
         novo_bloco->cabecalho->index_bucket = indice_bucket;
         bucket->blocos.push_back(novo_bloco);
         bucket->quatidade_blocos++;
-        cout << "Novo bloco criado" << endl;
+        //cout << "Novo bloco criado" << endl;
         inserir_registro_bucket(hashtable, registro);
         return;
     }else{
@@ -200,7 +241,11 @@ int main(int argc, char const *argv[])
 
     // Escrita dos buckets no arquivo de dados
     for (int i = 0; i < NUM_BUCKETS; i++) {
-        dataFile.write((char*)&hashTable->buckets[i], sizeof(Bucket));
+        for(int j = 0; j < hashTable->buckets[i]->quatidade_blocos; j++){
+            Bloco* bloco = hashTable->buckets[i]->blocos.front();
+            hashTable->buckets[i]->blocos.pop_front();
+            dataFile.write((char*) bloco->dados, sizeof(bloco->dados));
+        }
     }
     cout << "Arquivo de dados criado com sucesso!" << endl;
 
